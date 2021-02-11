750504aa08450f23551e1f42aa6537baab4580af
Martin Najjar <martin_najjar@yahoo.com>
2021-02-11 20:59:48 +0300

    Create the components files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
+function App() {
+  return (
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Display extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}

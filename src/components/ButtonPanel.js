c260c3a25a5d29125d9510036afc545dad81394d
Martin Najjar <martin_najjar@yahoo.com>
2021-02-12 17:48:17 +0300

    Center the calculator and style it with Material UI

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,33 +4,33 @@ import Button from './Button';
 const ButtonPanel = () => (
   <>
     <div>
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" />
+      <Button name="AC" topPanel="true" />
+      <Button name="+/-" topPanel="true" />
+      <Button name="%" topPanel="true" />
+      <Button name="÷" rightPanel="true" />
     </div>
     <div>
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
-      <Button name="×" />
+      <Button name="×" rightPanel="true" />
     </div>
     <div>
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
-      <Button name="-" />
+      <Button name="−" rightPanel="true" />
     </div>
     <div>
       <Button name="1" />
       <Button name="2" />
       <Button name="3" />
-      <Button name="+" />
+      <Button name="+" rightPanel="true" />
     </div>
     <div>
-      <Button name="0" />
+      <Button name="0" zeroButton="true" />
       <Button name="." />
-      <Button name="=" />
+      <Button name="=" rightPanel="true" />
     </div>
   </>
 );
